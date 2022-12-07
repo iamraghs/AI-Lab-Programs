7aff4ba9fc0b3d3d26bd72faeb894c9d17197afc
Raghavendra Kurdekar <53779288+iamraghs@users.noreply.github.com>
2022-12-07 19:43:16 +0530

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,1 +1,97 @@
 
+def printboard(board):
+      print(board[1] + '|' + board[2] + '|' + board[3])
+      print('-+-+-')
+      print(board[4] + '|' + board[5] + '|' + board[6])
+      print('-+-+-')
+      print(board[7] + '|' + board[8] + '|' + board[9])
+      print("\n")
+
+
+def spaceIsFree(position):
+    if board[position] == ' ':
+        return True
+    else:
+        return False      
+
+
+#printboard(board)      
+def insertLetter(letter, position):
+    if spaceIsFree(position):
+        board[position] = letter
+        printboard(board)
+        if (checkDraw()):
+            print("Draw!")
+            exit()
+        if checkForWin():
+            if letter == 'X':
+                print("Player2 wins!")
+                exit()
+            else:
+                print("Player1 wins!")
+                exit()
+
+        return
+
+
+    else:
+        print("Can't insert there!")
+        position = int(input("Please enter new position:  "))
+        insertLetter(letter, position)
+        return
+
+
+def checkForWin():
+    if (board[1] == board[2] and board[1] == board[3] and board[1] != ' '):
+        return True
+    elif (board[4] == board[5] and board[4] == board[6] and board[4] != ' '):
+        return True
+    elif (board[7] == board[8] and board[7] == board[9] and board[7] != ' '):
+        return True
+    elif (board[1] == board[4] and board[1] == board[7] and board[1] != ' '):
+        return True
+    elif (board[2] == board[5] and board[2] == board[8] and board[2] != ' '):
+        return True
+    elif (board[3] == board[6] and board[3] == board[9] and board[3] != ' '):
+        return True
+    elif (board[1] == board[5] and board[1] == board[9] and board[1] != ' '):
+        return True
+    elif (board[7] == board[5] and board[7] == board[3] and board[7] != ' '):
+        return True
+    else:
+        return False
+
+def checkDraw():
+    for key in board.keys():
+         if(board[key]==' ') :
+             return True
+         return False    
+
+
+
+def playerMove():
+    position = int(input("Enter the position for 'O':  "))
+    insertLetter(player, position)
+    return
+def compmove():
+     position = int(input("Enter the position for 'X':  "))
+     insertLetter(bot, position)
+     return  
+
+#driver code
+board = {1: ' ', 2: ' ', 3: ' ',
+         4: ' ', 5: ' ', 6: ' ',
+         7: ' ', 8: ' ', 9: ' '}
+printboard(board)
+player ='O'
+bot = 'X'
+
+print("Positions are as follow:")
+print("1, 2, 3 ")
+print("4, 5, 6 ")
+print("7, 8, 9 ")
+print("\n")
+
+while not checkForWin():
+    compmove()
+    playerMove()
